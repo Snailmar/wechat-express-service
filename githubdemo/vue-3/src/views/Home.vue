f20d65190624ba9a1511a34905035a95617352e7
vigor <vigorzhang@163.com>
2020-11-01 15:41:21 +0800

    init

---
+++ githubdemo/vue-3/src/views/Home.vue
@@ -0,0 +1,28 @@
+<!--
+ * @Author: vigorzhang
+ * @Date: 2020-11-01 15:35:41
+ * @LastEditors: Riddick
+ * @LastEditTime: 2020-11-01 15:39:04
+ * @Description: 
+-->
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  setup(){
+    console.log('set')
+  }
+}
+</script>
